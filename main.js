bbe2a2acf33a4ff99497a41705347f9d80c22248
Akshay Tallikar <67356708+AkshayTallikar@users.noreply.github.com>
2024-03-05 17:51:13 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-// **Instructions** **main.js**
-// ------------
-// In this document you will find instructions on how to adjust different parameters of the paradigm. You can apply the desired changes to the document main.js on your computer or server, using a source code editor.
-// The following parameters are necessary to adjust: number of avatar images, and the redirect link at the end of the study. All other parameters have a default option and adjustments are optional.
-
 $(function() {
-  // **Parameters**
-  // ------------
-
+  //GLOBAL VARIABLES
   var totalLink = 'https://osu.az1.qualtrics.com/jfe/form/SV_eWY4YOSQN3iwdFQ?';
   var globalUsername = "";
   var globalAvatar = "";
@@ -15,45 +8,33 @@ $(function() {
   var countDislike = 0;
   function set_settings() {
     window.settings = [];
-
-	  // **Number** **of** **Avatar** **Images**
-	  // Number of avatars the user can choose from. Can be changed to any number, depending on how many avatars you would like to display. Default: 82
-	  // The avatar images used in the online preview of the paradigm were created using by pickaface.net and due to their terms not available for redistribution. You should therefore create your own images. All images should be 250x250 pixels in size and carry the names "avatar_NUMBER.png" (e.g. avatar_1.png; "png" should be lower case; the numbers in the names should be consequtive, starting from 1). The number of avatars dependeds on the corresponding parameter. The images should be placed in folder "avatars," located in the main study folder extracted on your computer or server.
     settings.numberofavatars = 82;
-
-    // **Redirection**
-	  // After the introduction task is over participants should be redirected to a survey with manipulation checks and dependent measures, to subsequent tasks, or to further instructions.
-	  // If the study is called with a parameter for redirection, as explained in the documentation, this value is overwritten.
-	  // To the redirect link, the following information will be appended: (1) participant number, (2) condition, (3) username, (4) description submitted by participant. These variables can be extracted from the link, saved as data, and used for linking the Social Media Ostracism paradigm to subsequent tasks and measures. See documentation for more details.
     settings.defaultredirect = 'https://osu.az1.qualtrics.com/jfe/form/SV_eWY4YOSQN3iwdFQ';
-
-	  // **Tasklength**
-    // Length of the group introduction task in milliseconds. Can be changed to any number (in ms). Default: 180000 (3min)
-    settings.tasklength = 180000;    // Original time
-
-    // **Number** **of** **"dislikes"**
-    // Each received "like" is indicated by the timepoint (in ms) at which the "like" will appear. To change the number of "likes" in each condition, add or remove timepoints. Make sure that every timepoint (except the first) is preceded by a single comma.
-    // User will receive 6 likes at the following timepoints (in ms).
+    settings.tasklength = 180000;
     window.settings.condition_likes = [10000,20000,35000,45000,60000,78000,80000,100000,132000];
-
-	  // **Others' likes**
-	  // To keep the total distribution of "likes" constant across conditions, The "likes" received by one group member can be adjusted according to the participant's. By default, the other group member receives 9 "likes" in the participant-ostracism condition, 5 in the participant-inclusion condtion, and 1 in the participant-overinclusion condtion.
-    window.others.posts[1].likes = [12000,14000,15000,35000,80000]; // 5
-    window.others.posts[1].Dislikes = [12000,14000,15000,35000,80000]; // 5
-
-    // Usernames by which the participant will receive "likes"
-	  // If group member names are changed, these should be changed accordingly.
-    settings.likes_by = ['Ky', 'Arjen', 'AncaD', 'Nick', 'Heather', 'Jane', 'Georgeee', 'John',  'Mary'];
+    window.settings.condition_Dislikes = []
+    window.others.posts[1].likes = [12000,14000,15000,35000,80000];
+    window.others.posts[1].Dislikes = [12000,14000,15000,35000,80000];
+    settings.likes_by = ['Ky', 'Arjen', 'AncaD', 'Nick', 'Heather', 'Jane', 'Georgeee', 'John',  'Mary', 'Lauren', 'Sarah'];
     settings.Dislikes_by = [ 'Lauren', 'Arjen', 'Jane',  'Ky', 'AncaD', 'Nick', 'Heather', 'Georgeee', 'John', 'Mary', 'Sarah'];
+    window.query_string =null;
   }
-
+  
   // -------------------
-  // Above were the basic parameters you can adjust using the instructions. The remaining code is also annotated, but we do not recommend changing it, unless you are comfortable with web programming.
+  // Above were the basic parameters you can adjust using the instructions.
+  // The remaining code is also annotated, but we do not recommend changing it, unless you are comfortable with web programming.
   // -------------------
 
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  // Generate a random integer between 1 and 4 (inclusive)
+  const randomNumber = getRandomInt(1, 4);
+  console.log(randomNumber);
 
-  // **Slide:** **Intro**
-  // With instructions regarding the task. The intro container is shown, the continue calls the next slide when clicked.
+  //**Intro**
+  // Instructions regarding the task//
   function init_intro() {
   	$('#intro').show();
   	$('#submit_intro').on('click',function() {
@@ -62,36 +43,39 @@ $(function() {
   	});
   }
 
-  // **Slide:** **Username**
-  // Note: Only alphanumeric usernames without spaces are accepted
+  //**Username**
+  //Only alphanumeric usernames//
+function init_name() {
+  const nameContainer = $('#name');
+  const usernameInput = $('#username');
+  const submitButton = $('#submit_username');
 
-  function init_name() {
+  nameContainer.show();
 
-	$('#name').show();
-  	$('#submit_username').on('click',function() {
-  		var error = 0;
-  		var uname = $('#username').val();
+  submitButton.on('click', () => {
+    let error = 0;
+    let errormsg = '';
+    const uname = usernameInput.val().trim();
 
-  		if(uname == "") {
-  		  error = 1;
-  		  errormsg = 'Please enter text';
-  		  uname = "undefined";
-  		}
-  		if(not_alphanumeric(uname)) {
-  			error = 1;
-  		  errormsg = 'Please only letters (and no spaces)';
-  		}
-  		if(error == 0) {
-        $('#name').hide();
-        window.username = $('#username').val();
-        globalUsername = $('#username').val();
-        totalLink += "username=" + globalUsername;
-        init_avatar();
-      } else {
-        alertify.log(errormsg,"error");
-  		}
-  	});
-  }
+    if (!uname) {
+      error = 1;
+      errormsg = 'Please enter text';
+    } else if (not_alphanumeric(uname)) {
+      error = 1;
+      errormsg = 'Please only letters (and no spaces)';
+    }
+
+    if (error === 0) {
+      nameContainer.hide();
+      window.username = uname;
+      globalUsername = uname;
+      totalLink += "username=" + globalUsername;
+      init_avatar();
+    } else {
+      alertify.log(errormsg, 'error');
+    }
+  });
+}
 
 
   // **Slide:** **Avatar**
@@ -159,16 +143,6 @@ $(function() {
         init_fb_intro();
       }
     });
-  
-    // Function to display error messages and show an alert for less than 200 characters
-    function showError(message) {
-      $('#error_message').text(message);
-  
-      // If the entered text is less than 200 characters, show an alert
-      if ($('#description').val().length < 200) {
-        alert('Please note: You entered less than 200 characters.');
-      }
-    }
   }
 
 
@@ -198,19 +172,22 @@ $(function() {
   	});
   }
 
-  // Disables the like/dislike buttons and notifies the user that the time is up.
+  // Disables the like buttons and notifies the user that the time is up.
   function DeactivateLike() {
 	  setTimeout(function() { 
       $('.btn-like').attr("disabled", true);
+      alertify.log("Time is up").maximize();
     }, 3000);
   }
+
+  // Disables the dislike buttons and notifies the user that the time is up.
   function DeactivateDisLike(){
     setTimeout(function() {   
       $('.btn-Dislike').attr("disabled", true);
     }, 3000);
   }
 
-  // **Slide:** **Task**
+  //**Task**
   function init_task() {
     $('#task').show();
     shortcut.add("Backspace",function() {});
@@ -226,7 +203,6 @@ $(function() {
         totalLink += "&likes=" + countlike;
         totalLink += "&dislikes=" + countDislike;
         const timerLength = 10000; 
-        // const redirectUrl = "https://osu.az1.qualtrics.com/jfe/form/SV_eWY4YOSQN3iwdFQ";
         setTimeout(() => {
           window.location.href = totalLink;
         }, timerLength);
@@ -325,6 +301,7 @@ $(function() {
         }
       }
     });
+
     // Initialize like buttons
     $('.btn-like').on('click', function() {
       $(this).prev().text(parseInt($(this).prev().text()) + 1);
@@ -343,18 +320,6 @@ $(function() {
       $(this).parent().parent().find('.btn-Dislike').attr("disabled", true);
     });
 
- 
-    // $('.btn-like').onclick = function () {
-      
-    //   countlike++;
-    //   alert(countlike);
-    // }
-
-    // $('.btn-Dislike').onclick = function () {
-    //   countDislike++;
-    //   alert(countDislike)
-    // }
-
     // Initalize Masonry plugin
     // For display of user and other players boxes in columns without gaps
     $('#task').masonry({
@@ -365,53 +330,9 @@ $(function() {
     // Redirect, default after 180000ms = 180s = 3min, disabled 'Continue' button with redirect link
     setTimeout(function() {
       $(window).unbind('beforeunload');
-      //$('#final-continue').show();
       $('#timer').text('00:00');
-      //$('#final-continue').on('click', function() {
-        // Redirect link
-        //location.href = window.redirect+'&a='+window.participant+'&b='+window.condition+'&c='+encodeURI(window.username)+'&d='+window.avatarexport+'&e='+encodeURI(window.description);  // change p->a, c->b, u ->c, av->d, d->e
-      //});
     },window.settings.tasklength); // timing for task
   }
-
-  // Sets redirect link for string query, MUST ENCODE REDIRECT LINK
-  function get_params() {
-    if(window.QueryString.redirect !== undefined && window.QueryString.redirect !== "") {
-      window.redirect = decode(window.QueryString.redirect);
-    } else {
-      window.redirect = window.settings.defaultredirect;
-    }
-
-    var urlHasQuestionMark = (window.redirect.indexOf("?") > -1);
-
-    if(!urlHasQuestionMark) {
-      window.redirect = window.redirect+"?redir=1";
-    }
-  }
-
-  // The variable QueryString contains the url parameters, i.e. condition no. and participant no.
-  // via http://stackoverflow.com/a/979995
-  window.QueryString = function () {
-    var query_string = {};
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-      var pair = vars[i].split("=");
-        // If first entry with this name
-      if (typeof query_string[pair[0]] === "undefined") {
-        query_string[pair[0]] = pair[1];
-        // If second entry with this name
-      } else if (typeof query_string[pair[0]] === "string") {
-        var arr = [ query_string[pair[0]], pair[1] ];
-        query_string[pair[0]] = arr;
-        // If third or later entry with this name
-      } else {
-        query_string[pair[0]].push(pair[1]);
-      }
-    }
-      return query_string;
-  } ();
-
 
   // Function to check letters and numbers
   // via http://www.w3resource.com/javascript/form/letters-numbers-field.php
@@ -429,16 +350,6 @@ $(function() {
   function pad (str, max) {
       return str.length < max ? pad("0" + str, max) : str;
   }
-
-  // Function for encoding and decoding URLs
-  // via http://meyerweb.com/eric/tools/dencoder/
-  function encode(unencoded) {
-    return encodeURIComponent(unencoded).replace(/'/g,"%27").replace(/"/g,"%22");
-  }
-  function decode(encoded) {
-    return decodeURIComponent(encoded.replace(/\+/g,  " "));
-  }
-
 
   // Simple Countdown
   // via http://davidwalsh.name/jquery-countdown-plugin
@@ -479,7 +390,6 @@ $(function() {
 
   // Set Settings
   set_settings();
-  get_params();
 
   // Start with the intro slide
   init_intro();
